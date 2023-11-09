fb344318cddf16d49c195772ff3cf5d4bb931bee
Reemayg1999 <129390817+Reemayg1999@users.noreply.github.com>
2023-11-09 19:11:13 +0530

    Delete CICD/stack01_iam/01.Bastion/bastion_module directory

--- CICD/stack01_iam/01.Bastion/bastion_module/local.tf
+++
@@ -1,5 +0,0 @@
-locals{
-    compartment_ocid     = data.oci_identity_compartments.this.compartments[0].id
-    target_subnet_ocid   = data.oci_core_subnets.this.subnets[0].id
-    
-}
--- CICD/stack01_iam/01.Bastion/bastion_module/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "oci" {
-  region           = var.region
-  # ignore_defined_tags = ["Oracle-Tags.CreatedBy", "Oracle-Tags.CreatedOn"]
-}
-
-provider "oci" {
-alias = "home"
-region = "${lookup(data.oci_identity_regions.home-region.regions[0], "name")}"
-}
